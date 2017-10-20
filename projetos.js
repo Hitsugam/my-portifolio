6cfd1f8f39afad0ec93a88001a9d63e6adc1bac1
Lucas Gonçalves <front.end.lb@gmail.com>
2017-10-20 15:35:29 -0200

    Add files via upload

---
+++ projetos.js
@@ -0,0 +1,44 @@
+//abrindo modal
+
+function projetoOne() {
+	document.getElementById("bg-modal-meu").style.display = "flex";
+	document.getElementById("projeto-um").style.display = "block";
+}
+
+function projetoTwo() {
+	document.getElementById("bg-modal-meu").style.display = "flex";
+	document.getElementById("projeto-dois").style.display = "block";
+}
+
+function projetoTree() {
+	document.getElementById("bg-modal-meu").style.display = "flex";
+	document.getElementById("projeto-tres").style.display = "block";
+}
+
+function projetoFour() {
+	document.getElementById("bg-modal-meu").style.display = "flex";
+	document.getElementById("projeto-quatro").style.display = "block";
+}
+
+function projetoFive() {
+	document.getElementById("bg-modal-meu").style.display = "flex";
+	document.getElementById("projeto-cinco").style.display = "block";
+}
+
+function projetoSix() {
+	document.getElementById("bg-modal-meu").style.display = "flex";
+	document.getElementById("projeto-seis").style.display = "block";
+}
+
+
+//fechando modal
+
+function closeModal() {
+	document.getElementById("bg-modal-meu").style.display = "none";
+	document.getElementById("projeto-um").style.display = "none";
+	document.getElementById("projeto-dois").style.display = "none";
+	document.getElementById("projeto-tres").style.display = "none";
+	document.getElementById("projeto-quatro").style.display = "none";
+	document.getElementById("projeto-cinco").style.display = "none";
+	document.getElementById("projeto-seis").style.display = "none";
+}
